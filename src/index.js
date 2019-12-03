d734be2e6e5b71c1b37bb0d9aa6034ccc5006d4f
Dmytro Barylo <bdpochta@gmail.com>
2019-12-03 15:13:58 +0200

    Fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import {} from "dotenv/config";
+import { } from "dotenv/config";
 import Telegraf from "telegraf";
 import searchBooks from "./searchBooks";
+
+const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || 'https://books-and-bot.herokuapp.com/';
 
 const createMessageText = book => {
   return `
@@ -9,33 +13,35 @@ const createMessageText = book => {
 <a href="${book.cover_url}">&#8205;</a>`;
 };
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf(BOT_TOKEN);
 bot.on("inline_query", async ctx => {
   const searchResults = await searchBooks(ctx.inlineQuery.query);
   const results =
     searchResults && searchResults.length
       ? searchResults.map((book, id) => ({
-          id,
-          type: "article",
-          title: book.title,
-          description: book.author,
-          thumb_url: book.thumb_url,
-          input_message_content: {
-            message_text: createMessageText(book),
-            parse_mode: "HTML"
-          },
-          reply_markup: {
-            inline_keyboard: [
-              [
-                {
-                  text: "Show on Goodreads",
-                  url: book.url
-                }
-              ]
+        id,
+        type: "article",
+        title: book.title,
+        description: book.author,
+        thumb_url: book.thumb_url,
+        input_message_content: {
+          message_text: createMessageText(book),
+          parse_mode: "HTML"
+        },
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "Show on Goodreads",
+                url: book.url
+              }
             ]
-          }
-        }))
+          ]
+        }
+      }))
       : [];
   ctx.answerInlineQuery(results);
 });
+bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
+bot.startWebhook(`/bot${BOT_TOKEN}`, null, PORT);
 bot.launch();
