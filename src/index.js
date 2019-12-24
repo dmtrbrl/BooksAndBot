134d44afba4f560f651e5a15c0166089f8a1372e
Dmytro Barylo <bdpochta@gmail.com>
2019-12-25 00:09:53 +0200

    Set and start webhook

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import { } from "dotenv/config";
+import {} from "dotenv/config";
 import Telegraf from "telegraf";
 import searchBooks from "./searchBooks";
 
-const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const BOT_TOKEN = process.env.BOT_TOKEN || "";
 const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || "https://books-and-bot.herokuapp.com/";
 
 const createMessageText = book => {
   return `
@@ -18,28 +19,28 @@ bot.on("inline_query", async ctx => {
   const results =
     searchResults && searchResults.length
       ? searchResults.map((book, id) => ({
-        id,
-        type: "article",
-        title: book.title,
-        description: book.author,
-        thumb_url: book.thumb_url,
-        input_message_content: {
-          message_text: createMessageText(book),
-          parse_mode: "HTML"
-        },
-        reply_markup: {
-          inline_keyboard: [
-            [
-              {
-                text: "Show on Goodreads",
-                url: book.url
-              }
+          id,
+          type: "article",
+          title: book.title,
+          description: book.author,
+          thumb_url: book.thumb_url,
+          input_message_content: {
+            message_text: createMessageText(book),
+            parse_mode: "HTML"
+          },
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: "Show on Goodreads",
+                  url: book.url
+                }
+              ]
             ]
-          ]
-        }
-      }))
+          }
+        }))
       : [];
   ctx.answerInlineQuery(results);
 });
-bot.startWebhook('/', null, PORT);
-bot.launch();
+bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
